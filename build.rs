7edaa87dbd5007a4dabb1f3f6920809c242e02a9
Gregory Mitrano <gregory.mitrano@gmail.com>
2020-07-01 01:38:03 -0400

    Make Binding Generation an Optional Feature
    This should allow the crate to build properly on Windows setups that
    don't have clang installed and also significantly reduces build times.

--- build.rs
+++ build.rs
@@ -2,29 +2,24 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
-fn main() {
-    let lib_path = PathBuf::from("lib");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+#[cfg(target_os = "windows")]
+pub const LIB_FILE_NAME: &str = "ftd2xx.lib";
 
-    let lib_name = if cfg!(target_os = "linux") {
-        "libftd2xx.a"
-    } else {
-        "ftd2xx.lib"
-    };
-    fs::copy(
-        lib_path.join(lib_name).to_str().unwrap(),
-        out_path.join(lib_name).to_str().unwrap(),
-    )
-    .expect("Failed to copy native lib to output directory");
+#[cfg(not(target_os = "windows"))]
+pub const LIB_FILE_NAME: &str = "libftd2xx.a";
 
-    println!("cargo:rustc-link-lib=static=ftd2xx");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_path.to_str().unwrap()
-    );
+#[cfg(target_os = "windows")]
+pub const PREBUILT_BINDINGS_FILE_NAME: &str = "bindings_win.rs";
 
+#[cfg(not(target_os = "windows"))]
+pub const PREBUILT_BINDINGS_FILE_NAME: &str = "bindings_lnx.rs";
+
+pub const BINDINGS_FILE_NAME: &str = "bindings.rs";
+
+// If binding generation is enabled, run bindgen to generate fresh bindings
+#[cfg(feature = "gen-bindings")]
+fn process_bindings(_lib_path: &PathBuf, out_path: &PathBuf) {
     println!("cargo:rerun-if-changed=lib/ftd2xx.h");
-
     let bindings = bindgen::Builder::default()
         .header("lib/ftd2xx.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -37,6 +32,36 @@ fn main() {
         .expect("Unable to generate bindings");
 
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file(out_path.join(BINDINGS_FILE_NAME))
         .expect("Couldn't write bindings!");
+}
+
+// If binding generation is not enabled, copy the prebuilt bindings from the lib folder
+#[cfg(not(feature = "gen-bindings"))]
+fn process_bindings(lib_path: &PathBuf, out_path: &PathBuf) {
+    fs::copy(
+        lib_path.join(PREBUILT_BINDINGS_FILE_NAME).to_str().unwrap(),
+        out_path.join(BINDINGS_FILE_NAME).to_str().unwrap(),
+    )
+    .expect("Failed to copy prebuilt bindings to output directory");
+}
+
+fn main() {
+    let lib_path = PathBuf::from("lib");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    // Copy the static library into the output folder and instruct cargo to link against it
+    fs::copy(
+        lib_path.join(LIB_FILE_NAME).to_str().unwrap(),
+        out_path.join(LIB_FILE_NAME).to_str().unwrap(),
+    )
+    .expect("Failed to copy native lib to output directory");
+
+    println!("cargo:rustc-link-lib=static=ftd2xx");
+    println!(
+        "cargo:rustc-link-search=native={}",
+        out_path.to_str().unwrap()
+    );
+
+    process_bindings(&lib_path, &out_path);
 }
